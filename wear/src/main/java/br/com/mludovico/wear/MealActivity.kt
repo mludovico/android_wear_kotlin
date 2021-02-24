9bcf1c65bd55a66b4114317e1b57d92814ea713f
Marcelo Ludovico <maludovico@msn.com>
2021-02-23 22:54:11 -0300

    Finish preparing environment. Running on both devices

--- wear/src/main/java/br/com/mludovico/wear/MealActivity.kt
+++ wear/src/main/java/br/com/mludovico/wear/MealActivity.kt
@@ -1,15 +1,13 @@
 package br.com.mludovico.wear
 
+import android.app.Activity
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
 
-class MealActivity : WearableActivity() {
+class MealActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meal)
-
-        // Enables Always-on
-        setAmbientEnabled()
     }
 }
